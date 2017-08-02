ffe527ebc9427caa3009192507eea8f0ee78a6fe
Yuta Akizuki <shyss.ak+dev@gmail.com>
2017-08-02 11:43:08 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const bitcoin = require('bitcoinjs-lib')
-const isTest  = true
 
-let config = isTest ? {
-  NETWORK_MODE:    bitcoin.networks.testnet,
-  BALANCE_API:     'https://testnet.blockexplorer.com/api/addr/ADDRESS/balance',
-  UTXO_API:        'https://testnet.blockexplorer.com/api/addr/ADDRESS/utxo',
-  TRANSACTION_API: 'https://testnet.blockexplorer.com/api/tx/send',
-  IS_TEST:         true
-} : {
+let config = process.env.NODE_ENV == 'production' ? {
   NETWORK_MODE: bitcoin.networks.bitcoin,
   BALANCE_API:     'https://blockexplorer.com/api/addr/ADDRESS/balance',
   UTXO_API:        'https://blockexplorer.com/api/addr/ADDRESS/utxo',
   TRANSACTION_API: 'https://blockexplorer.com/api/tx/send',
   IS_TEST:         false
+} : {
+  NETWORK_MODE:    bitcoin.networks.testnet,
+  BALANCE_API:     'https://testnet.blockexplorer.com/api/addr/ADDRESS/balance',
+  UTXO_API:        'https://testnet.blockexplorer.com/api/addr/ADDRESS/utxo',
+  TRANSACTION_API: 'https://testnet.blockexplorer.com/api/tx/send',
+  IS_TEST:         true
 }
 
 module.exports = config
